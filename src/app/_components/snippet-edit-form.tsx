d07003e0e8b42eee5210a7c5bf82b35b5bd2abad
Sujan Timalsina <sujan.timalsina@apexcollege.edu.np>
2023-12-07 09:54:37 +0545

    (wip -> edit snippet form) setting up monaco editor

--- src/app/_components/snippet-edit-form.tsx
+++ src/app/_components/snippet-edit-form.tsx
@@ -1,9 +1,31 @@
 "use client";
 import type { Snippet } from "@prisma/client";
+import { Editor } from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
-  return <>{JSON.stringify(snippet)}</>;
+  const [code, setCode] = useState(snippet.code);
+
+  function handleEditorChange(value: string = "") {
+    setCode(value);
+  }
+
+  return (
+    <>
+      <div className="container p-8 mx-auto">
+        <h1 className="text-indigo-600 text-xl my-8">{snippet.title}</h1>
+        <Editor
+          height="40vh"
+          theme="vs-dark"
+          language="javascript"
+          defaultValue={snippet.code}
+          options={{ minimap: { enabled: false } }}
+          onChange={handleEditorChange}
+        />
+      </div>
+    </>
+  );
 }
